8ee6eca5af4aca918dab05e47ebece4d18a5c372
Vasily-drov <vasily.drov@gmail.com>
2022-03-13 00:13:14 +0300

    second

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,20 +9,14 @@ import {NavLink} from "react-router-dom";
 
 function Home() {
 
-    gsap.set("h1 div", {yPercent:-103})
-    const tl = gsap.timeline()
-    const { useEffect, useRef } = React;
-    tl.to("h1 div", {duration:1, yPercent:0, stagger:0.05, ease:"expo.inOut"})
-
-        .to("h1 div:not([data-char='.'])", {duration:1, yPercent:103, stagger:0.1, ease:"expo.inOut"})
-
+  
     const boxRef = useRef();
 
     useEffect(() => {
         gsap.to(boxRef.current, { rotation: "+=360" });
     });
 
-    
+
     return (
 
 
