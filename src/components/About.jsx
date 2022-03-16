ffb710e47c5d71ad4e421772ebc3add961e686a4
Kiril <kiril@Kirils-MacBook-Pro.local>
2022-03-16 14:29:59 -0400

    design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { gsap } from "gsap";
-
 const { useEffect } = React;
 const tl = gsap.timeline()
 
 function About() {
 
+    
+
     useEffect(() => {
         
-        tl.fromTo("#v_b", {x:-600}, {x:0, duration:1, ease:"power3.out"})
-        tl.to("#v_b", {
+        tl.fromTo("#v_b", {x:0, y:0}, {
             duration: 3,
             x: -150,
             y: 100,
-            repeat: -1,
-            stagger: 2,
             yoyo: true,
-            ease: "sine.inOut"
+            repeat: Infinity,
+            ease: "sine.out"
         });
+        
+        
+        
     
       });
 
@@ -29,16 +31,17 @@ function About() {
                 <div className="col-lg-4">
                     <img id="v_b"src="/about/v_b.png" class="img-fluid" alt="..."></img>
                 </div>
-                <div className="col-lg-7 offser-lg-1">
+                <div className="col-lg-7">
                     <h1 className="philosophy shimmer"> PHILOSOPHY</h1>
                     <h1 className="philosophy shimmer" id="cyber"> OF CYBER</h1>
                     <h1 className="philosophy shimmer" id="renaissance"> RENAISSANCE</h1>
                 </div>
+                
                     
                 </div>
                 <div className="row">
 
-                    <p className="fs-3 my-3">
+                    <p className="fs-3 my-3 ph">
                         Renaissance itself wasn’t only about brilliant artists and philosophers.
                         It was also about philanthropists and benefactors who supported all these geniuses and together were making a difference in the world.
                         There’s no shortage of money to do the sorts of things they did,
--- src/components/AboutExt.jsx
+++ src/components/AboutExt.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const { useEffect } = React;
 const tl = gsap.timeline()
+
+gsap.registerPlugin(ScrollTrigger);
 
 function AboutExt() {
 
     useEffect(() => {
         
-        tl.fromTo("#v_b", {x:-600}, {x:0, duration:1, ease:"power3.out"})
-        tl.to("#v_b", {
-            duration: 3,
-            x: -150,
-            y: 100,
-            repeat: -1,
-            stagger: 2,
-            yoyo: true,
-            ease: "sine.inOut"
-        });
+        tl.fromTo("#venus", {x:-800, y:-700, opacity:0}, {
+            x:-400,
+            y:-100,
+            opacity:1, 
+            duration: 5,
+            ease:"none",
+        })
+
+        tl.fromTo("#greg", {x:100,y:80,opacity:0, scale:0}, {
+            x:-200,
+            y:0,
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            ease:"back.out"
+        })
+
+        tl.fromTo("#liza", {x:0, opacity:0, scale:0}, {
+            x:400,
+            y:0,
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            ease:"back.out"
+        })
+
+        tl.fromTo("#vasya", {x:100, y:100, opacity:0, scale:0}, {
+            x:0,
+            y:0,
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            ease:"back.out"
+        })
+
+        
     
       });
 
@@ -26,27 +55,47 @@ function AboutExt() {
         <div className="aboutext">
             <div className="container pageCont">
                 <div className="row my-5">
-                <div className="col-lg-4">
-                    <img id="v_b"src="/about/v_b.png" class="img-fluid" alt="..."></img>
-                </div>
-
-                </div>
-
-                <div className="row our-team">
+                    <div className="col-lg-5">
+                        <img id="venus"src="/about/venus.png" class="img-fluid" alt="..."></img>
+                    </div>
+                    <div className="col-lg-7">
                     <h2>OUR TEAM:</h2>
 
-                        <div className="col-lg-4">
-                            
-                            <a href="/about">Gregg Watermann</a>
+                    
+
+                        <div className="card our-team" id="greg">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Gregg Watermann</h5> 
+                                <p className="card-text">fghjbknml,;</p>  
+                                <a href="/about">link</a>
+                            </div>
                         </div>
-                        <div className="col-lg-4">
-                            <a href="/about">Elizabeth Mendez</a>
+                        <br />
+                        <div className="card our-team" id="liza">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Elizabeth Mendez</h5> 
+                                <p className="card-text">fghjbknml,;</p>  
+                                <a href="/about">link</a>
+                            </div>
                         </div>
-                        <div className="col-lg-4">
-                            <a href="/about">Vasily Drovetskiy</a>
+                        <br />
+                        <div className="card our-team" id="vasya">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Vasily Drovetskiy</h5> 
+                                <p className="card-text">fghjbknml,;</p>  
+                                <a href="/about">link</a>
+                            </div>
                         </div>
+                        
+
+                    </div>
 
                 </div>
+
+                
             </div>
         </div>
     );
