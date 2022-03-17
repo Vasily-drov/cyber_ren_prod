f4b7e887ba83e9c95b955155bfcea95b6e9675e2
Kiril <kiril@Kirils-MacBook-Pro.local>
2022-03-17 18:15:48 -0400

    vfdhj

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Portfolio, About,OurServices, Contact, AnimationPortfolio, VideoProdPortfolio, WebDevelopment, Home } from './components'
+import {Portfolio, Contact, AnimationPortfolio, VideoProdPortfolio, WebDevelopment, Home } from './components'
 
 
 
